cd280aa0647b036f0c4b1195d035edbd95c01032
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-11-28 13:17:47 -0500

    Added some new controllers for the system

---
+++ src/server/system/notificationController.js
@@ -0,0 +1,7 @@
+
+/*
+	This module is responsible for taking care of notifications and distributing them.
+ */
+module.exports = function() {
+	
+}
---
+++ src/server/system/questionTransitController.js
@@ -0,0 +1,7 @@
+/*
+	The question transit controller is responsible for managing the transmission of all the various notifications that need to be placed.
+ */
+ */
+module.exports = function() {
+	
+}
