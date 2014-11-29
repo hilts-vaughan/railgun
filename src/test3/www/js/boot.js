1df8fd76e75b3707d92d60c501e00661958e5eb9
Vaughan Hilts <vhilts@axonify.com>
2014-11-28 22:11:30 -0500

    Cleaned up code seperation

---
+++ src/test3/www/js/boot.js
@@ -0,0 +1,74 @@
+  var app = angular.module('myApp', ['ionic', 'ngCordova']);
+
+  app.run(function($http) {
+      $http.defaults.headers.common['auth'] = localStorage['identity'];
+  });
+
+  app.value('config', {
+      serverUrl: 'http://localhost:8080/'
+  });
+
+
+  app.config(function($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/login')
+
+      $stateProvider
+        .state('index', {
+            url: '/home',
+            templateUrl: 'home.html',
+            controller: "HomeController"
+        })
+
+
+      .state('eula', {
+          url: '/eula',
+          templateUrl: 'eula.html'
+      })
+
+      .state('alerts', {
+          url: '/alerts',
+          templateUrl: 'alerts.html'
+      })
+
+      .state('login', {
+          url: '/login',
+          templateUrl: 'login.html',
+          controller: 'LoginController'
+      })
+
+      .state('help', {
+          url: '/help',
+          templateUrl: 'help.html'
+      })
+
+      .state('post', {
+          url: '/post/:id',
+          templateUrl: 'post.html',
+          controller: "PostItem"
+      })
+
+      .state('topiclist', {
+          url: '/topiclist/:id',
+          templateUrl: 'topiclist.html',
+          controller: "TopicList"
+      })
+
+      .state('reply', {
+          url: '/reply/:id/:title',
+          templateUrl: 'reply.html',
+          controller: "ReplyItem"
+      })
+
+      .state('postlist', {
+          url: '/postlist/:id',
+          templateUrl: 'postlist.html',
+          controller: "PostList"
+      })
+
+      .state('profile', {
+          url: '/profile',
+          templateUrl: 'profile.html'
+      });
+
+  });
---
+++ src/test3/www/js/controllers.js
@@ -0,0 +1,161 @@
+app.controller('PostItem', function($scope, $http, $stateParams, $location, config) {
+
+      var category = $stateParams.id;
+      $scope.params = $stateParams;
+
+
+      $scope.SubmitPost = function(title, body) {
+
+          var question = {
+              title: title,
+              body: body,
+              categoryId: category
+          };
+
+          $http.post(config.serverUrl + 'submissions/questions', question).
+          success(function(data, status, headers, config) {
+              console.log(data);
+              $location.path("/postlist/" + data.id);
+          }).
+          error(function(data, status, headers, config) {
+              alert("Connection Failed");
+          });
+      };
+  })
+
+
+
+  app.controller('ReplyItem', function($scope, $http, $stateParams, $location, config) {
+      var id = $stateParams.id;
+      var title = $stateParams.title
+      $scope.Title = title;
+      $scope.params = $stateParams;
+
+      $scope.SubmitReply = function(body) {
+
+          var question = {
+
+              body: body,
+
+          };
+
+          $http.post(config.serverUrl + 'submissions/answers/' + id, question).
+          success(function(data, status, headers, config) {
+              $location.path("/postlist/" + data.parentQuestionId);
+          }).
+          error(function(data, status, headers, config) {
+              alert("Connection Failed");
+          });
+      };
+
+
+
+
+  })
+
+  app.controller('LoginController', function($scope, $http, $stateParams, $cordovaOauth, $location, config) {
+
+
+
+
+      $scope.login = function() {
+          // Do stuff
+          localStorage["identity"] = $scope.identity;
+
+          $scope.checkIdentity();
+      }
+
+      $scope.checkIdentity = function() {
+          var identity = localStorage["identity"];
+          if (identity) {
+
+              $http.defaults.headers.common['auth'] = identity;
+
+              $http.get(config.serverUrl + 'login/' + identity).
+              success(function(data, status, headers, config) {
+                  $location.path("/home");
+                  console.log(data);
+              }).
+              error(function(data, status, headers, config) {
+                  alert("Connection Failed");
+              });
+
+
+          }
+      }
+
+      $scope.checkIdentity();
+
+  })
+
+
+  app.controller('TopicList', function($scope, $http, $stateParams, config) {
+
+      var category = $stateParams.id;
+      $scope.params = $stateParams;
+
+      $http.get(config.serverUrl + 'submissions/questions?categoryId=' + category).
+      success(function(data, status, headers, config) {
+          $scope.names = data;
+          console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+          alert("Connection Failed");
+      });
+
+  })
+
+  app.controller('PostList', function($scope, $http, $stateParams, $ionicPopup, config) {
+
+      var questionId = $stateParams.id;
+      $scope.params = $stateParams;
+
+      $http.get(config.serverUrl + 'submissions/questions/' + questionId).
+      success(function(data, status, headers, config) {
+          console.log(data);
+          $scope.names = data;
+      }).
+      error(function(data, status, headers, config) {
+          alert("Connection Failed");
+      });
+
+
+      $scope.show_Popup = function() {
+          $scope.data = {}
+              // An elaborate, custom popup
+          var myPopup = $ionicPopup.show({
+              title: 'Report Post',
+              subTitle: 'Please select a report reason.',
+              scope: $scope,
+              buttons: [{
+                  text: '<font size="1">Spam</font>',
+                  onTap: function(e) {
+                      alert('Spam - Post ID:' + $scope.names._id);
+                  }
+              }, {
+                  text: '<font size="1">Language</font>',
+                  onTap: function(e) {
+                      alert('Language - Post ID:' + $scope.names._id);
+                  }
+              }, {
+                  text: '<font size="1">Cancel</font>',
+                  onTap: function(e) {}
+              }, ]
+          });
+          myPopup.then(function(res) {
+              console.log('Tapped!', res);
+          });
+
+      };
+
+  });
+
+
+
+  app.controller('HomeController', function HomeController($scope, $http) {
+
+      $scope.data = {
+          category: 0
+      };
+
+  });
